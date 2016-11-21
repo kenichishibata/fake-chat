9f8ad8c1c0d6f94fc2e989b46b74b8f15ca95101
kenichishibata <kenichi.shibata3123@gmail.com>
2016-11-21 13:37:00 +0900

    removed: babel-cli dependency for server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, '/../build')));
 
 const PORT = process.env.PORT || 8888;
 app.listen(PORT, () =>
